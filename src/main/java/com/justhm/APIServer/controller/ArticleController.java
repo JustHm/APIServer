0bbe2b69b5f439f83d1892fa612ba097d6380d85
JustHm <taggma12@gmail.com>
2023-04-11 15:02:30 +0900

    article Get Delete 추가

--- src/main/java/com/justhm/APIServer/controller/ArticleController.java
+++ src/main/java/com/justhm/APIServer/controller/ArticleController.java
@@ -3,11 +3,11 @@ package com.justhm.APIServer.controller;
 import com.justhm.APIServer.model.Article;
 import com.justhm.APIServer.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 //@Controller
 @RestController
@@ -20,6 +20,22 @@ public class ArticleController {
     @PostMapping("/article")
     public Boolean articleSave(@RequestBody Article article) {
         articleRepository.save(article);
+        return true;
+    }
+
+    @GetMapping("/articles")
+    public List<Article> articleList(@RequestParam int page, @RequestParam int num) {
+        List<Article> list = articleRepository.findAll();
+        return list;
+    }
+
+    @GetMapping("/article")
+    public Optional<Article> getArticle(@RequestParam BigInteger id) {
+        return articleRepository.findById(id);
+    }
+    @DeleteMapping("/article/del")
+    public Boolean deleteArticle(@RequestParam BigInteger id) {
+        articleRepository.deleteById(id);
         return true;
     }
 }
